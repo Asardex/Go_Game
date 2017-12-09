02f4be67822637117dce1551a5a004b23d6f7f05
Quentin Brossier <Quentinbrossier.t7@gmail.com>
2017-12-09 15:27:22 +0100

    Création du projet Java Maven
    Création avec une classe principale, utilisant les dépendences jUnit et log4j

---
+++ Go_Game/src/main/java/Go_Game/App.java
@@ -0,0 +1,13 @@
+package Go_Game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
