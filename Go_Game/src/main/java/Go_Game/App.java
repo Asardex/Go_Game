86345dcc6dcf3d3b0a63bd92e1c333948a4bef8b
Quentin Brossier <Quentinbrossier.t7@gmail.com>
2017-12-09 18:51:02 +0100

    Création du lancement de la partie

--- Go_Game/src/main/java/Go_Game/App.java
+++ Go_Game/src/main/java/Go_Game/App.java
@@ -1,21 +1,62 @@
 package Go_Game;
+
+import java.util.Scanner;
+
 /**
  * Classe App, point de lancement de l'application
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+
+	public static void main( String[] args )
     {
-    	Joueur J1 = new Joueur(Couleur.Blanc);
-    	Joueur J2 = new Joueur(Couleur.Blanc);
-    	regles();
+    	Scanner sc;
+    	int choix = 0;
+    
+    	System.out.print("Bienvenu dans le Goban. ");
+    	do {
+    		System.out.println("Quel voulez vous faire ?\n\t1. Jouer\n\t2. Lire les règles\n\t3. Comment jouer ?");
+	    	sc = new Scanner(System.in);
+	    	choix = sc.nextInt();
+	    	switch(choix) {
+	    		case 1 :
+	    			lancerPartie();
+	    			break;
+	    		case 2:
+	    			regles();
+	    			break;
+	    		case 3:
+	    			commentJouer();
+	    			break;
+    			default:
+    				break;
+	    	}
+    	}while(choix != 1);
     	
+
+    	sc.close();
+    	System.out.println("\nA bientôt !");
     	return;
     }
     
-    public static void regles() {
-    	System.out.println("Deux camps s'opposent pour obtenir le plus grand territoire : "
+    private static void lancerPartie() {
+    	Joueur J1 = new Joueur(Couleur.Blanc);
+    	Joueur J2 = new Joueur(Couleur.Noir);
+    	
+    	System.out.println("\nDebut de la partie.");
+    	
+    	
+    	System.out.println("Fin de la partie.");
+		
+	}
+
+	private static void commentJouer() {
+    	System.out.println("\nEntrez les coordonées de la pierre que vous voulez placer sous la forme (1;19).\n\n");
+	}
+
+	public static void regles() {
+    	System.out.println("\nDeux camps s'opposent pour obtenir le plus grand territoire : "
     			+ "les pierres blanches et les pierres noires.\n"
     			+ "Les joueurs peuvent passer leur tour.\n"
     			+ "Si les deux joueurs passent leur tour l'un apres l'autre, "
@@ -29,7 +70,7 @@ public class App
     			+ "si une pierre, ou un groupe de pierres est entouré "
     			+ "par des pierres de l'autre couleur, les pierres entourées sont capturées, enlevée du "
     			+ "plateau, et le nombre final de pierres capturées sera ajouté au score du joueur qui les "
-    			+ "a capturés");
+    			+ "a capturés\n\n");
     	
     	return;
     }
